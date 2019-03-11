ee36f14e6e8cbee921012009ad704ba48a0dd56f
PPAmaranth <523002128@qq.com>
2019-03-11 19:37:50 +0800

    react

--- src/page/main.js
+++ src/page/main.js
@@ -1,3 +1,23 @@
+import { Row,Col,Layout } from 'antd';
+
+const {
+  Header, Footer, Sider, Content,
+} = Layout;
+
+
+
 export default () => {
-  return <div>hello world</div>;
+  return (
+	  	<Layout style={{height:"100vh","width":"100vw"}}>
+    		<Content>main content</Content>
+	      <Footer>
+	      	<Row gutter={24}>
+			  <Col span={6} >1</Col>
+			  <Col span={6} >2</Col>
+			  <Col span={6} >3</Col>
+			  <Col span={6} >4</Col>
+			</Row>
+	      </Footer>
+	    </Layout>
+  	);
 }
