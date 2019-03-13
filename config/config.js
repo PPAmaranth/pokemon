c11e7752a5cad6aa03a5220431b5d51b2c703ff3
PPAmaranth <523002128@qq.com>
2019-03-13 19:37:45 +0800

    Signed-off-by: PPAmaranth <523002128@qq.com>

--- config/config.js
+++ config/config.js
@@ -2,9 +2,22 @@ export default {
   singular: true,
   plugins: [
     ['umi-plugin-react', {
-      antd: true
+      antd: true,
+      dva:true
     }],
   ],
+  lessLoaderOptions:{
+    rules: [{
+      test: /\.less$/,
+      use: [{
+        loader: 'style-loader' // creates style nodes from JS strings
+      }, {
+        loader: 'css-loader' // translates CSS into CommonJS
+      }, {
+        loader: 'less-loader' // compiles Less to CSS
+      }]
+    }]
+  },
   routes: [{
     path: '/',
     component: './main',
--- src/page/main.js
+++ src/page/main.js
@@ -1,23 +1,34 @@
-import { Row,Col,Layout } from 'antd';
-
+import "antd/dist/antd.css";
+import { Layout,Tabs } from 'antd';
+import mainLess from '../style/main.less'
 const {
   Header, Footer, Sider, Content,
 } = Layout;
 
+class MainTab extends React.Component {
+  state = {
+    tabPosition: 'bottom',
+  }
+  render(){
+  	const TabPane = Tabs.TabPane;
+  	return(
+  		<Tabs tabPosition={this.state.tabPosition} className={mainLess.mainTab}>
+          <TabPane className={mainLess.test3} tab="Tab 1" key="1">Content of Tab 1</TabPane>
+          <TabPane tab="Tab 2" key="2">Content of Tab 2</TabPane>
+          <TabPane tab="Tab 3" key="3">Content of Tab 3</TabPane>
+        </Tabs>
+  	);
+  }
+}
 
 
 export default () => {
+  console.log(mainLess)
   return (
 	  	<Layout style={{height:"100vh","width":"100vw"}}>
-    		<Content>main content</Content>
-	      <Footer>
-	      	<Row gutter={24}>
-			  <Col span={6} >1</Col>
-			  <Col span={6} >2</Col>
-			  <Col span={6} >3</Col>
-			  <Col span={6} >4</Col>
-			</Row>
-	      </Footer>
+    		<Content className={mainLess.content}>
+    			<MainTab id="test3"></MainTab>
+    		</Content>
 	    </Layout>
   	);
 }
