5e83dc8daf5e60f3aa42239121e2f3b910523f84
PPAmaranth <523002128@qq.com>
2019-04-24 17:23:01 +0800

    properties canvas

--- src/page/modPage/illustratedHandbook/detail.js
+++ src/page/modPage/illustratedHandbook/detail.js
@@ -1,23 +1,112 @@
 import { Component } from 'react';
 // 引入 ECharts 主模块
 import echarts from 'echarts/lib/echarts';
-//引入雷达
-import  'echarts/lib/chart/radar';
+import 'echarts/lib/component/tooltip';
+import 'echarts/lib/component/legend';
+import 'echarts/lib/chart/radar';
+import 'echarts/lib/chart/pie';
 import { List } from '@/component/list.js';
 import pageLess from './index.less';
 import {pm_propeties} from '@/public_js/properties.js';
 const notfoundPng = require("@/image/system/notFound.png");
+
+class PropertiesCounteredTable extends Component{
+	constructor(props){
+        super(props);
+    }
+    componentDidMount(){
+    	//玫瑰图
+    	let _propertiesCounteredEchart = echarts.init(document.getElementById("detailPropertiesCounteredEchart"));
+        let _legendData = []
+        let _colorData = []
+        for(var i in pm_propeties){
+        	if(pm_propeties[i]['CNname']){
+        		_legendData.push(pm_propeties[i]['CNname'])
+        		_colorData.push(pm_propeties[i]['color'])
+        	}
+        }
+        let _counteredData = pm_propeties[this.props.state.currentItem.propertyOne]['countered'];
+        if(this.props.state.currentItem.propertyTwo){
+        	//双属性计算
+        	let newCounteredData = {}
+        	let _counteredDataTwo = pm_propeties[this.props.state.currentItem.propertyTwo]['countered'];
+        	for(let i in _counteredData){
+        		newCounteredData[i] = _counteredData[i] * _counteredDataTwo[i]
+        	}
+        	_counteredData = newCounteredData
+        }
+        let _echartsData = []
+        for(let i in _counteredData){
+        	let _obj = {
+        		value:_counteredData[i],
+        		name:pm_propeties[i]['CNname']
+        	}
+        	_echartsData.push(_obj)
+        }
+        const option = {
+		    title : {},
+		    tooltip : {
+		        trigger: 'item'
+		    },
+		    color:_colorData,
+		    legend: {
+		        x : 'center',
+		        y : 'bottom',
+		        data:_legendData
+		    },
+		    toolbox: {
+		        show : true,
+		        feature : {
+		            mark : {show: true},
+		            dataView : {show: true, readOnly: false},
+		            magicType : {
+		                show: true,
+		                type: ['pie', 'funnel']
+		            },
+		            restore : {show: true},
+		            saveAsImage : {show: true}
+		        }
+		    },
+		    calculable : true,
+		    series : [
+		        {
+		            name:this.props.state.currentItem.name,
+		            type:'pie',
+		            radius : [0, 90],
+		            center : ['50%', '30%'],
+		            roseType : 'area',
+		            data:_echartsData
+		        }
+		    ]
+		};
+        _propertiesCounteredEchart.setOption(option);
+		window.resizeCallbacks = {
+			...window.resizeCallbacks,
+			_propertiesCounteredEchart:{
+				..._propertiesCounteredEchart
+			}
+		}
+    }
+    render(){
+        return (
+        	<div style={{width:"100%",height:"100%"}} id="detailPropertiesCounteredEchart"></div>
+        )
+    }
+}
 
 class AbilityTable extends Component{
 	constructor(props){
         super(props);
     }
     componentDidMount(){
+    	//雷达图
     	let _detailAbilityEchart = echarts.init(document.getElementById("detailAbilityEchart"));
-        console.log(this.props)
         const option = {
 		    title: {},
-		    tooltip: {},
+		    tooltip: {
+		    	trigger: 'item',
+		    	position: 'left'
+		    },
 		    legend: {},
 		    radar: {
 		        name: {
@@ -28,25 +117,38 @@ class AbilityTable extends Component{
 		        nameGap: 0,
 				radius: "75%",
 		        indicator: [
-		           { name: 'HP', max: 200},
-		           { name: '攻击', max: 200},
-		           { name: '防御', max: 200},
-		           { name: '特攻', max: 200},
-		           { name: '特防', max: 200},
-		           { name: '速度', max: 200}
+		           { name: 'HP', max: 255 , color: '#98FB98'},
+		           { name: '攻击', max: 255 , color: '#bbb'},
+		           { name: '防御', max: 255 , color: '#FF7F50'},
+		           { name: '速度', max: 255 , color: '#DDA0DD'},
+		           { name: '特防', max: 255 , color: '#3A5FCD'},
+		           { name: '特攻', max: 255 , color: '#7EC0EE'}
 		        ]
 		    },
 		    series: [{
-		        name: '',
+		        name: this.props.state.currentItem.name,
 		        type: 'radar',
 		        data : [
 		            {
-		                value : [45, 49, 49, 65, 65, 45],
+		                value : [
+			                this.props.state.currentItem.hp, 
+			                this.props.state.currentItem.attack,
+			                this.props.state.currentItem.defense,
+			                this.props.state.currentItem.speed,
+			                this.props.state.currentItem.sDefense,
+			                this.props.state.currentItem.sAttack
+		                ]
 		            }
 		        ]
 		    }]
 		};
         _detailAbilityEchart.setOption(option);
+		window.resizeCallbacks = {
+			...window.resizeCallbacks,
+			_detailAbilityEchart:{
+				..._detailAbilityEchart
+			}
+		}
     }
     render(){
         return (
@@ -147,6 +249,14 @@ export class Detail extends Component{
 			</div>
 		)
 	}
+	//属性克制
+	renderPropertiesCountered(state){
+		return(
+			<div className={pageLess.propertiesCountered}>
+				<PropertiesCounteredTable state={state}></PropertiesCounteredTable>
+			</div>
+		)
+	}
 	//风琴折叠页
 	renderCollapseItem(state,item){
 		let _contentHeight
@@ -193,7 +303,8 @@ export class Detail extends Component{
 	        },
 	        {
 	          title:"属性克制",
-	          index:3
+	          index:3,
+	          render:this.renderPropertiesCountered
 	        },
 	        {
 	          title:"进化关系",
@@ -224,6 +335,17 @@ export class Detail extends Component{
 				</div>
 			</div>
 		)
+	}
+	componentWillMount(){
+		//多个resize事件
+		window.resizeCallbacks = {}
+		window.onresize=function(){
+		  for(let i in window.resizeCallbacks){
+		  	if(window.resizeCallbacks[i].resize){
+		  		window.resizeCallbacks[i].resize()
+		  	}
+		  }
+		}
 	}
 	render(){
 		return (
